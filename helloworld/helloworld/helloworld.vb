2645a9b566e0d44b7035f619fbf8ffbe044303f1
jtrusnovec <60245181+jtrusnovec@users.noreply.github.com>
2020-01-30 20:12:22 -0500

    Hello World with Message Box and Button
    Added btnPress to form that triggers Message Box that says hello world to the user.

---
+++ helloworld/helloworld/helloworld.vb
@@ -0,0 +1,7 @@
+﻿Public Class helloworld
+    Private Sub btnPress_Click(sender As Object, e As EventArgs) Handles btnPress.Click
+        'show a message box
+        MessageBox.Show("Hello World!")
+
+    End Sub
+End Class
