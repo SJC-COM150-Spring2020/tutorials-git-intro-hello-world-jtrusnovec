beadef72d590d1cb817576339e234222c2e646e7
jtrusnovec <60245181+jtrusnovec@users.noreply.github.com>
2020-01-30 20:28:40 -0500

    printed hello world to text box
    used txtName to show Hello World message.

--- helloworld/helloworld/helloworld.vb
+++ helloworld/helloworld/helloworld.vb
@@ -1,7 +1,13 @@
 ﻿Public Class helloworld
     Private Sub btnPress_Click(sender As Object, e As EventArgs) Handles btnPress.Click
         'show a message box
-        MessageBox.Show("Hello World!")
+        'MessageBox.Show("Hello World!")
+
+        'say hello world in a text box
+        txtName.Text = "Hello World!"
+
+
+
 
     End Sub
 End Class
